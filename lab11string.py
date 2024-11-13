1941858438a589dd17876d528d4633d436cb1220
Joswen.Pinca <owenpinca2022@gmail.com>
2024-11-13 13:58:54 +0800

    Lab_11

--- lab11string.py
+++ lab11string.py
@@ -1,6 +1,12 @@
-words=["dogs", "cats", "birds", "fish", "rabbits", "hamsters", "pigs", "mice", "rats", "turtles", "snakes"]
-
+word_count=0
+words=[]
+matched_words=[]
+while word_count<10:
+    word=input("Enter a word: ")
+    words.append(word)
+    word_count+=1
 num=int(input("Enter the number of letters you want to input to display the words under the same number of letters: "))
 for i in words:
     if len(i)>=num:
-        print(i)
+        matched_words.append(i)
+print(matched_words)
